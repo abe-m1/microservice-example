ffc4b25af088d482920c69515205ec4384b47ca9
abe menashy <abestbet1@gmail.com>
2020-12-02 14:07:00 -0800

    crud for ticketing

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,12 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { errorHandler, NotFoundError} from '@abemtickets/common'
 
+import { errorHandler, NotFoundError, currentUser } from '@abemtickets/common'
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 //traffic is being proxied to the application through ingress-nginx
@@ -23,11 +23,14 @@ app.use(
     secure: process.env.NODE_ENV !== 'test'
   })
 );
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
+
+
 
 app.all('*', async (req, res, next) => {
   //using express-async-errors
